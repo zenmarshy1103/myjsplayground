f00d52f739b9c69d01065529ff33573c2677a4dc
Jason Liu <104143535+zenmarshy1103@users.noreply.github.com>
2023-03-30 08:42:22 +1300

    The Conditionals (Tenary Operator)
    // The Conditionals (Ternary Operator)
    // - Another way of if-else statement when the code being executed of the conditional is simple and not complicated
    // - Can be confusing but once understood it is very useful is come cases.
    // - SYNTAX: variable = if statement ? if true execute this : else execute this

--- starter/script.js
+++ starter/script.js
@@ -1,38 +1,21 @@
-//The Switch Statement 
-// - Another way of conditional statement
-// - Used to compare one value through multiple different options.
-// - Tidier and easier approach than if - else conditional statement (at times)
+// The Conditionals (Ternary Operator)
+// - Another way of if-else statement when the code being executed of the conditional is simple and not complicated
+// - Can be confusing but once understood it is very useful is come cases.
+// - SYNTAX: variable = if statement ? if true execute this : else execute this
 
-let whatIsToday = prompt(`What day is it today: `);
+var age = prompt('What is your age? ')
 
-whatIsToday = whatIsToday.toLowerCase();
-console.log(`Today is ${whatIsToday}`);
+const drink = age >= 18 ? "wine": 'water';
+console.log(drink);
 
-switch(whatIsToday) {
-    //Case for if only monday 
-    case 'monday':
-        console.log('1. Plan for this week');   //Similar to if (monday)
-        console.log('2. Work  Traininig');
-        console.log('3. Weight Training')
-        break;    //Break is needed if this is the only block to run for the case condition
-    //Case for if wednesday or friday    
-    case 'wednesday':
-    case 'friday':         
-        console.log(`1. Work Training`);
-        console.log('2. Weight Training');
-        break;  
-    case 'tuesday':
-    case 'thursday':
-        console.log('1. Home Training');
-        console.log(`2. Work Training`);
-        console.log('3. Project Planning');
-        break;   //Without Braek the all the code following will run through until the next break is met
-    case 'saturday':
-    case 'sunday':
-        console.log('1. family Day');
-        console.log(`2. Rest`);
-        console.log('3. Junk Food Day');
-        break
-    default:  //Similar to else block ( else if day is not met for all the case conditions)
-        console.log('Not a valid day! please enter a valid weekday')
+//The above is similar to the following If-ELSE Statement
+
+let drink2;
+if (age >= 18) {
+    drink2 = 'wine';
+} else {
+    drink2 = 'water';
 }
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? "wine": 'water'}`)
