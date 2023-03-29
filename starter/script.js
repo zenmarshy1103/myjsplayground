59cc644e4f7f87d87da1a12d46082ea4bf381aa2
Jason Liu <104143535+zenmarshy1103@users.noreply.github.com>
2023-03-30 08:26:19 +1300

    The Switch Statement
    //The Switch Statement 
    // - Another way of conditional statement
    // - Used to compare one value through multiple different options.
    // - Tidier and easier approach than if - else conditional statement (at times)

--- starter/script.js
+++ starter/script.js
@@ -1,24 +1,38 @@
-// Truthy and Falsy Values (Boolean)
-// - Falsy: is not false and will become false when converted to a boolean (1. 0 2. '' 3. underfined 4. null 5. NaN)
-// - Truthy: will become true when converted to boolean (1. number > 0 , )
+//The Switch Statement 
+// - Another way of conditional statement
+// - Used to compare one value through multiple different options.
+// - Tidier and easier approach than if - else conditional statement (at times)
 
+let whatIsToday = prompt(`What day is it today: `);
 
-console.log(Boolean(0));  //Falsy Value
-console.log(Boolean('Jason')); //Truthy Value
-console.log(Boolean(``));     //Falsy Value
-console.log(Boolean(undefined));  //Falsy Value
-console.log(Boolean({})); //Truthy Value
+whatIsToday = whatIsToday.toLowerCase();
+console.log(`Today is ${whatIsToday}`);
 
-const money = 0;    
-if (money) {    //Boolean 0 = Falsy -> goes to else condition
-    console.log(`Don't spend it all`);
-} else {
-    console.log(`You should get a job`);
-}
-
-let height;   //height is undefined here as it is not assigned with any value. 
-if (height) {   //Boolean: undefine = Falsy -> goes to the else condition.
-    console.log(`Height is defined`);
-} else {
-    console.log(`Height is not defined`);
+switch(whatIsToday) {
+    //Case for if only monday 
+    case 'monday':
+        console.log('1. Plan for this week');   //Similar to if (monday)
+        console.log('2. Work  Traininig');
+        console.log('3. Weight Training')
+        break;    //Break is needed if this is the only block to run for the case condition
+    //Case for if wednesday or friday    
+    case 'wednesday':
+    case 'friday':         
+        console.log(`1. Work Training`);
+        console.log('2. Weight Training');
+        break;  
+    case 'tuesday':
+    case 'thursday':
+        console.log('1. Home Training');
+        console.log(`2. Work Training`);
+        console.log('3. Project Planning');
+        break;   //Without Braek the all the code following will run through until the next break is met
+    case 'saturday':
+    case 'sunday':
+        console.log('1. family Day');
+        console.log(`2. Rest`);
+        console.log('3. Junk Food Day');
+        break
+    default:  //Similar to else block ( else if day is not met for all the case conditions)
+        console.log('Not a valid day! please enter a valid weekday')
 }
