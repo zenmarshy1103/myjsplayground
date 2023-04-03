75ca6fa6b4fff515c297066bebf19d689587b99c
Jason Liu <104143535+zenmarshy1103@users.noreply.github.com>
2023-04-04 08:08:55 +1200

    Arrays - Methods
    //- Add element to array, remove element, 
    //- Identify the position of an element
    //- Checks if the element is in an array

--- script.js
+++ script.js
@@ -1,62 +1,43 @@
-// Arrays
-//- Data Structure
-//- Store Date in a big container to be used later in the program (can hold different data types at the same time)
-//- An array is not primitive data, so the elements can be mutated even when the array is declared as const
-//- An array cannot be replaced by another array
-'strict mode'
+// Arrays - Methods 
+//- Add element to array, remove element, 
+//- Identify the position of an element
+//- Checks if the element is in an array
 
-const friend1 = 'Jim';
-const friend2 = 'Allen';
-const friend3 = 'Leone';
+const friends = [`Allen`, `Jim`, `Han`, 'William', 'Willie'];
 
-//Create an array with three items
-const friends = ['Jim', 'Allen', 'Leone'];   //Literal Syntax
+//Push Method - Adds element to the end of an array
+const newLength = friends.push(`Nick`);    //A function(method) call will result in a returned data, push method returns the length of the new array or after the element has been pushed to the existing array
+console.log(friends);
+console.log(newLength);
+
+//Unshift Method - Adds element to the beginning of an array
+friends.unshift('Leone')  //Return data of the unshift method is also the new length of the array after adding in the element to the beginning of the array
 console.log(friends);
 
-//Another way of constructing an array with items
-const years = new Array(1991, 1989, 2023, 2000); 
-
-//Getting the elements of the items in the array
-console.log(friends[0]);  //First element in the array
-console.log(friends[2]); //Third element
-
-//Display the number of elements in the array
-console.log(friends.length)
-
-//Getting the last element of the array
-console.log(friends[friends.length -1]);
-
-//Mutate element in the array
-friends[2] = 'Jay';
+//Pop method - Removes the last element of an array
+friends.pop();
+const friendRemoved = friends.pop();  //Return data is the removed element
+console.log(friendRemoved);
 console.log(friends);
 
-//Illegal - AN ARRAY CANNNOT BE REPLACED BY ANOTHER ARRAY (Error in console will appear)
-// friends = ['Emma', 'Eliza'];
+//Shift Method - Removes the first element of an array
+friends.shift();
+const shifted = friends.shift();  //Return data is the removed element
+console.log(shifted);
+console.log(friends);
 
-//Arrays can have elements with different data type at the same time, string, data, calculation(int), array, function calls etc.
-const firstName = `Jason`;
-const jason = [firstName, 'Liu', 2023 - 1989, 'web developer', friends ];
-console.log(jason);
+//IndexOf Method - Identify the position of an element in an array
+console.log(friends.indexOf(`Jim`));
+console.log(friends.indexOf(`Frank`)); //If the element is not in the array, will return -1
 
-//Exercise - Using an array to be used in a function with single value input
-const calcAge = function (birthYear) {
-    return 2023 - birthYear;
+//Includes Method - Returns Boolean True / False if the element is in the array (Strict Equality)
+console.log(friends.includes(`Han`)); //True - is in the array
+console.log(friends.includes('Wang')); //False - is not in the array
+friends.push(23);
+console.log(friends.includes('23')); //False Does not do coersion 
+console.log(friends.includes(23)); //True
+
+if (friends.includes('Han')) {
+    console.log('You have a friend called Han');
 }
-
-const birthYears1 = [1989, 1993, 2022, 2023];
-
-const age1 = calcAge(birthYears1[0]);
-const age2 = calcAge(birthYears1[1]);
-const age3 = calcAge(birthYears1[birthYears1.length - 1]);
-console.log( age1, age2, age3);
-
-const ages = [calcAge(birthYears1[0]), calcAge(birthYears1[1]), calcAge(birthYears1[birthYears1.length - 1])];
-console.log(ages);
-
-
-
-
-
-
-
 
