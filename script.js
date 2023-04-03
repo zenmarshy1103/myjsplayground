03f1f1c214a667bcfa6c7d8aad3334720ca08ffb
Jason Liu <104143535+zenmarshy1103@users.noreply.github.com>
2023-04-04 08:22:08 +1200

    // Objects - Introduction
    //- Data Stucture
    //- Properties of Objects are Key: Value pairs
    //-

--- script.js
+++ script.js
@@ -1,43 +1,26 @@
-// Arrays - Methods 
-//- Add element to array, remove element, 
-//- Identify the position of an element
-//- Checks if the element is in an array
+// Objects - Introduction
+//- Data Stucture
+//- Properties of Objects are Key: Value pairs
+//-
 
-const friends = [`Allen`, `Jim`, `Han`, 'William', 'Willie'];
 
-//Push Method - Adds element to the end of an array
-const newLength = friends.push(`Nick`);    //A function(method) call will result in a returned data, push method returns the length of the new array or after the element has been pushed to the existing array
-console.log(friends);
-console.log(newLength);
+//Arrya Data Structure
+const jasonArray = [
+    'Jason',
+    'Liu',
+    2023 - 1989,
+    'web developer',
+    ['Allen', 'Jim', 'Han']
+];
 
-//Unshift Method - Adds element to the beginning of an array
-friends.unshift('Leone')  //Return data of the unshift method is also the new length of the array after adding in the element to the beginning of the array
-console.log(friends);
+//Constructing Object Data Sturcture with key-Value pairs
+const jason = {   //Object Literal Syntax - Literally constructing the object with the properties
+    // Assigning key: value pairs
+    firstName: 'Jason',  // property firstName , there are 5 properties for jason object
+    lastName: 'Liu',
+    age: 2023 - 1989,
+    job: 'web developer',
+    friends: ['Allen', 'Jim', 'Han'],
+};
 
-//Pop method - Removes the last element of an array
-friends.pop();
-const friendRemoved = friends.pop();  //Return data is the removed element
-console.log(friendRemoved);
-console.log(friends);
-
-//Shift Method - Removes the first element of an array
-friends.shift();
-const shifted = friends.shift();  //Return data is the removed element
-console.log(shifted);
-console.log(friends);
-
-//IndexOf Method - Identify the position of an element in an array
-console.log(friends.indexOf(`Jim`));
-console.log(friends.indexOf(`Frank`)); //If the element is not in the array, will return -1
-
-//Includes Method - Returns Boolean True / False if the element is in the array (Strict Equality)
-console.log(friends.includes(`Han`)); //True - is in the array
-console.log(friends.includes('Wang')); //False - is not in the array
-friends.push(23);
-console.log(friends.includes('23')); //False Does not do coersion 
-console.log(friends.includes(23)); //True
-
-if (friends.includes('Han')) {
-    console.log('You have a friend called Han');
-}
 
