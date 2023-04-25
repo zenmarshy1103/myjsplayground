1b325d3c40c1917b37ce6cc181cf507c754357cc
Jason Liu <104143535+zenmarshy1103@users.noreply.github.com>
2023-04-26 08:53:50 +1200

    Scoping
    // - Global ,functional Scope, block scope
    // - Functional Scope (parent scope)
    //

--- script.js
+++ script.js
@@ -1,38 +1,48 @@
-// While Loop
-//- The block will only execute when (while) the condition is true
-//- Counter need to be declared outside the while loop block (when Needed)
-//- Increment/decrement need to be put inside the while loop block (when Needed)
-//- Use when looping without the need of counter and incremented when trying to solve or execute anything without a counter or increment / decrement
-'strict mode'
+// Scoping 
+// - Global ,functional Scope, block scope
+// - Functional Scope (parent scope)
+//
 
-//For Loop
-for (let rep = 1; rep <= 10; rep++) {
-    console.log(`Lifting weight repetition ${rep}`);    
-}
-console.log(`------- End of For Loop -------`)
+'use strict';
 
-//Implementing the above for loop using while loop
-// While Loop - only specify the condition
-let rep = 1;
-while (rep <= 10) {
-    console.log(`Lifting weight repetition ${rep}`);
-    rep++
-}
-console.log(`------- End of While Loop -------`)
 
-// Example that does not depent on counter and counter increment or decrement
-let dice = Math.random() * 6; //calling Build-In Math function Outputs a decimal dumeber between 0 to 1 (multiplying 6 meaning will get from 0 - 6)
-dice = Math.trunc(dice) + 1;  //Truncating the decimal places (outputs 0 - 6),
-// console.log(dice)
+function calcAge(birthYear) {                 //This function is declared in the global scope
+    const age = 2023 - birthYear;
+    console.log(firstName);                   //firstName not in the scope of this function but it is a global variable
+    
+    function printAge() {
+        var millenial = false;
+        const firstName = 'Faye'   //This variable is now function scoped so the call back will use this instead of the global varible (when two of the same variables are being declared)
+        let output = `${firstName}, You are ${age}, born in ${birthYear}`;   // Age and birthYear variable out outside of the printAge function or in the parent of the printAge (Child) function
+        console.log(output);
 
-if (dice === 6){  //Added to check if 6 is rolled at the first time
-    console.log(`6 Is rolled, Loop eneded`);
-}
+        if (birthYear >= 1981 && birthYear <= 1996) {
+            const str = `your are a millenial ${firstName}`;
 
-while (dice !== 6) {
-    console.log(`Rolled a ${dice}`);
-    dice = Math.trunc(Math.random() * 6) + 1;
-    if (dice === 6){
-        console.log(`6 Is rolled, Loop eneded`);
+            function add(a, b) {
+                return a + b;
+            }
+
+            //output = "NEW OUPUTPUT" // This can be changed as output is in the parent scope
+            const output = " NEW OUTPUT";  //This will not be used when the output is called outside this scope as the parent scope variable will be used first during the lookup
+            
+        }
+
+        console.log(output);  // Now this prints "NEW OUTPUT"
+        //console.log(str);  //Reference Error - str variable is not global scope or function scope of the parent scope but it is a block scope.
+        console.log(millenial); // this runs as var variable is function scoped
+        //add(2, 3);   //Reference Error - as function add is blocked scope - ONLY in Strict Mode
+
+       
     }
+    printAge();
+
+    return age;
 }
+
+
+const firstName = 'Jason'                     //Global Variable
+
+calcAge(1989);
+
+//console.log(age);  //This will output referenceError - as age is function scope not a globally scoped variable
