9e93deb147f3de12fbcf2e9c0870e6128976a767
Jason Liu <104143535+zenmarshy1103@users.noreply.github.com>
2023-04-27 08:40:05 +1200

    Regular Function vs Arrow Function (this Keyword and arguments Keyword)
    // - NOTE: NEVER EVER use ARROW FUNCTION as a method of an object!!! this Keyword will always points to the window global object (Parent Scope) (widow)
    // - BUT: ARROW FUNCTION inside a method , this Keyword will get the parent scope object (window > object > object.method(with Arrow Function))
    // - NOTE: Function inside an object method, this Keyword will get undefined - 2 ways to fix: 1) Assign a varaible to preserve this Keyword of the object. 2) Use arrow function inside the object method.
    // - Argument keyword (Regular Functions: argument keyword gets the numbe of arguments entered, Arrow Function: does not have argument keyword)

--- script.js
+++ script.js
@@ -1,51 +1,72 @@
-// this Keyword
-// - Dynamic not static
-// - this will always points to the object calling the method
-// - ARROW FUNCTION: does not have thie keyword, this will get the window object.
-// -
+// Regular Function vs Arrow Function (this Keyword and arguments Keyword)
+// - NOTE: NEVER EVER use ARROW FUNCTION as a method of an object!!! this Keyword will always points to the window global object (Parent Scope) (widow)
+// - BUT: ARROW FUNCTION inside a method , this Keyword will get the parent scope object (window > object > object.method(with Arrow Function))
+// - NOTE: Function inside an object method, this Keyword will get undefined - 2 ways to fix: 1) Assign a varaible to preserve this Keyword of the object. 2) Use arrow function inside the object method.
+// - Argument keyword (Regular Functions: argument keyword gets the numbe of arguments entered, Arrow Function: does not have argument keyword)
 
 'use strict';
-// >> Global Variable
-console.log(this); // this gets the global object (window)
 
-//Function
-const calcAge = function (birthYear) {
-  console.log(2023 - birthYear);
-  console.log(this); // this gets the function this keyword which points to undefined
-  // as this referes to the function and it is not an object
-};
-calcAge(1989);
+var firstName = 'Jason' // NOTE: var creates an property on the window global object
 
-// >> ARROW FUNCTION
-const calcAgeArrow = birthYear => {
-  console.log(2023 - birthYear);
-  console.log(this); // NOTE: ARROW FUNCTION behaves differently to normal function
-  // ARROW FUNCTION: this gets the parent scope of the arrow function which is the window object
-  // Not the this keyword of its own arrow function.
-};
-calcAgeArrow(1989);
-
-// >> Object - Method
-// NOTE: this keyword always points to the the object that is calling the method
+// >> Arrow Function as method of an object
+// this keyword used by the arrow function will always get the global window object 
 const jason = {
-  year: 1989,
-  calcAge: function () {
-    console.log(this); // this points to the whole object with all its attributes
+    firstName: 'Jason',
+    year: 1989,
+    calcAge: function () {
+    //console.log(this); // this points to the whole object with all its attributes
     console.log(2023 - this.year); // this.attribute_of_class will let this points to the designated object attributes
-  },
+   
+    // // >> SOLUTION 1 - PRE ES6 (FOLLOW FIX 1, FIX 2 and FIX 3)
+    // // >> Making a self variable to use this Keyword to point to the object (preserving the this keyword)
+    // const self = this;  //FIX STEP 1 ( can also be const that = this) 
+
+    // // >> Adding a function in a method:
+    // const isMillenial = function() {
+    //     console.log(this)  // this Keyword gets undefined
+    //     console.log(self); //FIX STEP 2
+    //     console.log(self.year >= 1981 && self.year <= 1996); //FIX STEP 3
+    //     console.log(this.year >= 1981 && this.year <= 1996); //PIT FALL: this in a function will ALWAYS get undefined as it is just a regular function call
+    // }   
+    
+    
+    // >> SOLUTION 2 - ES6 Arrow function inside a object method - 
+    // >> Using an arrow function will get the parent scope object which is the jason object
+    const isMillenial = () => {       // FIX A  
+        console.log(this); 
+        console.log(this.year >= 1981 && this.year <= 1996); 
+    }    
+    isMillenial();
+    },
+
+  greet: () => console.log(`hey ${this.firstName}`),  // this is called by the greet function hence the arrow function will get the window function and window.firstName is undefined.
+  //But if a Var variable is created. this will get the var object when using the same var variable name.
+  greet1: () => console.log(`hey ${this}`),  // ARROW FUNCTION will get the window global object
 };
+jason.greet1();
+jason.greet();
+
+
+// > Function inside a method
+// this will ALWAYS get undefined even if a function is in the object (object > object.method > function)
 jason.calcAge();
 
-// >> this keyword always points to the the object that is calling the method
-const faye = {
-  year: 1993,
-};
-//adding object attribute (adding function)
-// Method borrowing
-faye.calcAge = jason.calcAge; // Assigning the calcAge function from jason object to faye object
-faye.calcAge(); // Now the this keyword gets faye object, NOTE: this keyword always points to the the object that is calling the method
+// To fix this, getting rid of undefined we need to set a self variable (PRE ES6 solution) (REFER SOLUTION 1: FIX 1, FIX 2 and FIX 3)
 
-// >> A function getting the function called from the method of an object
-// this keyword will only point to the object calling the method
-const f = jason.calcAge; //Copied the method to a variable (copying a funcion and assign to another function)
-f(); // this keyword points to undefined as there is no owning object just a regular function.
+// New way to fix ES6 solution (Refer to SOLUTION 2: FIX A - Using Arrow Function)
+
+
+// arguments keyword - Allow user to enter more argument into a function
+// arguments gets the number of argument inputs from the user specify and store them in an array
+const addExpr = function(a, b) {
+    console.log(arguments);
+    return a + b;
+}
+addExpr(2, 5);
+addExpr(2, 5 , 6, 7);
+
+var addArrow = (a, b) => {   // Arrow function does not have the argument keyword
+    console.log(arguments);
+    return a + b
+}
+addArrow(1, 3, 4, 5);
