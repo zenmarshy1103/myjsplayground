3957ca81c4bd98dad1e4180a6dc9b35aa20025c0
Jason Liu <104143535+zenmarshy1103@users.noreply.github.com>
2023-05-11 08:05:59 +1200

    Maps (ES6 - Data Structure) - Iteration
    // - Data Structure that we can map values to keys, keys in maps can be any type (Keys in an obejct are always string type)
    // - BUILD IN Methods: Create Map  <SYNTAX:> new Map()
    // - Add Element(usual and chaining) <SYNTAX:> .set(key, value)  or <SYNTAX:> .set(key, value).set(key, value).set(key, value) etc
    // - Read Data from maps using KEY <SYNTAX:> .get(key)
    // - Check a map contains certain KEY <SYNTAX:> .has(key)
    // - Remove all element from the map <SYNTAX:> .clear()
    // - Using ARRAY and map KEYS (NOTE: An array has to be initialised or the data will just get undefined - CAUSE: Different reference in the HEAP)
    // - Using OBJECT and map KEYS (ie using the DOM object as key using document.querySelector('html_element'))
    // - Converting an Object to Map
    // - Iteration - Getting Data out of a Map
    // - Convert Map to Array (use unpacking ... operator on Map)
    //    1. ([...question])    : unpacking the map to array
    //    2. ([...question.keys()])   : unpacking just the keys of the map to an array 
    //    3. ([...question.values()])   : unpacking just the values of the map to an array

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// Maps (ES6 - Data Structure) - Fundamentals
-// - Data Structure that we can map values to keys, keys in maps can be any type (Object keys are always string type)
+// Maps (ES6 - Data Structure) - Iteration
+// - Data Structure that we can map values to keys, keys in maps can be any type (Keys in an obejct are always string type)
 // - BUILD IN Methods: Create Map  <SYNTAX:> new Map()
 // - Add Element(usual and chaining) <SYNTAX:> .set(key, value)  or <SYNTAX:> .set(key, value).set(key, value).set(key, value) etc
 // - Read Data from maps using KEY <SYNTAX:> .get(key)
@@ -7,6 +7,12 @@
 // - Remove all element from the map <SYNTAX:> .clear()
 // - Using ARRAY and map KEYS (NOTE: An array has to be initialised or the data will just get undefined - CAUSE: Different reference in the HEAP)
 // - Using OBJECT and map KEYS (ie using the DOM object as key using document.querySelector('html_element'))
+// - Converting an Object to Map
+// - Iteration - Getting Data out of a Map
+// - Convert Map to Array (use unpacking ... operator on Map)
+//    1. ([...question])    : unpacking the map to array
+//    2. ([...question.keys()])   : unpacking just the keys of the map to an array 
+//    3. ([...question.values()])   : unpacking just the values of the map to an array
 
 'use strict';
 
@@ -65,69 +71,130 @@ const restaurant = {
     },
   },
 };
+// Maps - Iteration
 
-// >> Create a map  <SYNTAX:> new Map()
-const rest = new Map(); // Create an empty map (initially)
-const names = new Map();
+// Populating the map with out using .set()
 
-// >> Fill up the map (Add element to data stucture)  <SYNTAX:> .set()
-//  -  Sets contains Key and value pairs so Key and value need to be added at the same time
-rest.set('name', 'Classico Italiano');
-rest.set(1, 'Firenze, Itality');
-console.log(rest.set(2, 'Lisbon, protugal')); // .set will update the most recent set with the new element
-console.log(rest);
+// >> 1. Creating a new Map
+const question = new Map([
+  ['question', 'what is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct!!!'],
+  [false, 'Try again ~'],
+]);
+console.log(question); 
 
-// >> Chaining Maps (Creating elements one after the next) <SYNTAX:> .set().set().set()  etc
-rest
-  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
-  .set('open', 11)
-  .set('close', 23)
-  .set(true, 'We are open')
-  .set(false, 'we area closed');
-console.log(rest);
+// >> 2. Converting an Object to Map
+//    - When you have an object already and wanting to use its attributes and values in a easier access way or want to turn it into a map
+//    <STEPS>
+//    - 1. use Object.entries to turn Object into Array
+//    - 2. create a new Map using the array converted from STEP 1 <SYNTAX> new Map(Object.entries())
+console.log(Object.entries(restaurant.openingHours));  
+const hoursMap = new Map(Object.entries(restaurant.openingHours));
+console.log(hoursMap);
 
-// >> Read Data from maps  <SYNTAX:> .get()
-//  - Pass in the name of the key
-console.log(rest.get('name'));
-console.log(rest.get(true));
-console.log(rest.get(1));
+// >> 3. Iteration - Getting Data out of a Map
+//    >> BUILDING - QUIZ APP
+console.log(question.get('question'))  // Read the data that has the key 'question'
+for (const [key, value] of question ) {  // Using destructing to get key and value of the question map
+  if(typeof key === 'number') {
+    console.log(`Answer ${key}: ${value}`);
+  }
+}
 
-// Realtime usage
-const time = 21;
-// this example is clever but code may not be easily readable
-console.log(rest.get(time > rest.get('open') && time < rest.get('close'))); //Compare time against the time open and close in the map and return the value of true and false
+const answer = Number(prompt(`Please choose an answer from number 1 to 3`));
+console.log(answer);
+// - MY WAY 
+// if (answer === question.get('correct')) {
+//   console.log(question.get(true));  
+// } else {
+//   console.log(question.get(false));  
+// };
 
-// >> Check a map contains certain KEY <SYNTAX:> .has()
-console.log(rest.has('categories')); // Returns True if it has the key and false if it doesnt have it
+// - Better Solution (Having boolean values as key)
+// - Always work from the inner ( ) then work your way to the most outer (getting the logic of the inner () to work first then use the outer ())
+console.log(question.get(question.get('correct') === answer));
 
-// >> Deleting element in a map using the KEY <SYNTAX;> .delete()
-rest.delete(2);
-console.log(rest);
 
-// >> Check the size of the map <SYNTAX:> .size
-console.log(rest.size);
+// >> 4. Convert Map to Array (use unpacking ... operator on Map)
+console.log([...question]);  // Same is console.log([...question.entries()])
+console.log([...question.keys()]); // Unpack all the keys and store it into an array
+console.log([...question.values()]); // unpack all the values and store it into an array
 
-// >> Remove all element from the map <SYNTAX:> .clear()
-names.set('1', 'Jason').set(2, 'Faye');
-console.log(names);
-names.clear();
-console.log(names);
 
-// >> Using ARRAY and OBJECT and map KEYS
-// ARRAY as KEY
-rest.set([1, 2], 'Test');
-console.log(rest);
-// Retriving data using the KEY of ARRAY
-console.log(rest.get([1, 2])); // This array has its own idenfier (in the Heap), not the same as the one we construct in the .set()
 
-//To make the above work
-// - An array new to be made
-const arr = [1, 2]; // This will allow JS to always use the same array identifer in the Heap
 
-// - Pass in the made array into the map
-rest.set(arr, 'test1');
-console.log(rest.get(arr));
 
-// OBJECT as KEY (Using DOM object as Key) NOTE: DOM object is object type
-rest.set(document.querySelector('h1'), 'Heading');
-console.log(rest);
+
+
+// Maps - Fundamentals
+// // >> Create a map  <SYNTAX:> new Map()
+// const rest = new Map(); // Create an empty map (initially)
+// const names = new Map();
+
+// // >> Fill up the map (Add element to data stucture)  <SYNTAX:> .set()
+// //  -  Sets contains Key and value pairs so Key and value need to be added at the same time
+// rest.set('name', 'Classico Italiano');
+// rest.set(1, 'Firenze, Itality');
+// console.log(rest.set(2, 'Lisbon, protugal')); // .set will update the most recent set with the new element
+// console.log(rest);
+
+// // >> Chaining Maps (Creating elements one after the next) <SYNTAX:> .set().set().set()  etc
+// rest
+//   .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+//   .set('open', 11)
+//   .set('close', 23)
+//   .set(true, 'We are open')
+//   .set(false, 'we area closed');
+// console.log(rest);
+
+// // >> Read Data from maps  <SYNTAX:> .get()
+// //  - Pass in the name of the key
+// console.log(rest.get('name'));
+// console.log(rest.get(true));
+// console.log(rest.get(1));
+
+// // Realtime usage
+// const time = 21;
+// // this example is clever but code may not be easily readable
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close'))); //Compare time against the time open and close in the map and return the value of true and false
+
+// // >> Check a map contains certain KEY <SYNTAX:> .has()
+// console.log(rest.has('categories')); // Returns True if it has the key and false if it doesnt have it
+
+// // >> Deleting element in a map using the KEY <SYNTAX;> .delete()
+// rest.delete(2);
+// console.log(rest);
+
+// // >> Check the size of the map <SYNTAX:> .size
+// console.log(rest.size);
+
+// // >> Remove all element from the map <SYNTAX:> .clear()
+// names.set('1', 'Jason').set(2, 'Faye');
+// console.log(names);
+// names.clear();
+// console.log(names);
+
+// // >> Using ARRAY and OBJECT and map KEYS
+// // ARRAY as KEY
+// rest.set([1, 2], 'Test');
+// console.log(rest);
+// // Retriving data using the KEY of ARRAY
+// console.log(rest.get([1, 2])); // This array has its own idenfier (in the Heap), not the same as the one we construct in the .set()
+
+// //To make the above work
+// // - An array new to be made
+// const arr = [1, 2]; // This will allow JS to always use the same array identifer in the Heap
+
+// // - Pass in the made array into the map
+// rest.set(arr, 'test1');
+// console.log(rest.get(arr));
+
+// // OBJECT as KEY (Using DOM object as Key) NOTE: DOM object is object type
+// rest.set(document.querySelector('h1'), 'Heading');
+// console.log(rest);
+
+
