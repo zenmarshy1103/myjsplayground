54451bd617cf541d06a07f8a9168c02c49c80297
Jason Liu <104143535+zenmarshy1103@users.noreply.github.com>
2023-05-15 08:07:35 +1200

    Working with Strings - Part 1  (Slicing Strings)
    // - Slicing Strings 
    // - Strings are primitives, cannot be mutated
    // - when using method on string, JS automatically convers string into an object (Boxing)
    // - returned string from methods are primitives

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-// Maps (ES6 - Data Structure) - Iteration
-// - Data Structure that we can map values to keys, keys in maps can be any type (Keys in an obejct are always string type)
-// - BUILD IN Methods: Create Map  <SYNTAX:> new Map()
-// - Add Element(usual and chaining) <SYNTAX:> .set(key, value)  or <SYNTAX:> .set(key, value).set(key, value).set(key, value) etc
-// - Read Data from maps using KEY <SYNTAX:> .get(key)
-// - Check a map contains certain KEY <SYNTAX:> .has(key)
-// - Remove all element from the map <SYNTAX:> .clear()
-// - Using ARRAY and map KEYS (NOTE: An array has to be initialised or the data will just get undefined - CAUSE: Different reference in the HEAP)
-// - Using OBJECT and map KEYS (ie using the DOM object as key using document.querySelector('html_element'))
-// - Converting an Object to Map
-// - Iteration - Getting Data out of a Map
-// - Convert Map to Array (use unpacking ... operator on Map)
-//    1. ([...question])    : unpacking the map to array
-//    2. ([...question.keys()])   : unpacking just the keys of the map to an array 
-//    3. ([...question.values()])   : unpacking just the values of the map to an array
+// Working with Strings - Part 1  (Slicing Strings)
+// - Slicing Strings 
+// - Strings are primitives, cannot be mutated
+// - when using method on string, JS automatically convers string into an object (Boxing)
+// - returned string from methods are primitives
 
 'use strict';
 
@@ -71,130 +61,65 @@ const restaurant = {
     },
   },
 };
-// Maps - Iteration
+// String is very similar to ARRAY
+// - Strings are primitives so they cannot be mutated
 
-// Populating the map with out using .set()
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
 
-// >> 1. Creating a new Map
-const question = new Map([
-  ['question', 'what is the best programming language in the world?'],
-  [1, 'C'],
-  [2, 'Java'],
-  [3, 'JavaScript'],
-  ['correct', 3],
-  [true, 'Correct!!!'],
-  [false, 'Try again ~'],
-]);
-console.log(question); 
+console.log(plane[0]);  // Returns A
+console.log(plane[1]);  // Returns 3
+console.log(plane[3]);  // Returns 2
 
-// >> 2. Converting an Object to Map
-//    - When you have an object already and wanting to use its attributes and values in a easier access way or want to turn it into a map
-//    <STEPS>
-//    - 1. use Object.entries to turn Object into Array
-//    - 2. create a new Map using the array converted from STEP 1 <SYNTAX> new Map(Object.entries())
-console.log(Object.entries(restaurant.openingHours));  
-const hoursMap = new Map(Object.entries(restaurant.openingHours));
-console.log(hoursMap);
+console.log(`B737`[0]); // Returns B
 
-// >> 3. Iteration - Getting Data out of a Map
-//    >> BUILDING - QUIZ APP
-console.log(question.get('question'))  // Read the data that has the key 'question'
-for (const [key, value] of question ) {  // Using destructing to get key and value of the question map
-  if(typeof key === 'number') {
-    console.log(`Answer ${key}: ${value}`);
+console.log(airline.length);
+console.log('B737'.length);
+
+// >> Sting Methods
+//  <BOXING> 
+//  when using method on string, JS automatically convers string into an object (Boxing)
+//  <Behind the Scene>
+console.log(new String('TAP Air Portugal'));
+console.log(typeof(new String('TAP Air Portugal')));
+// Then the sting object will have build in methods (see console terminal and expand the object)
+// All returned string from method are primitive 
+console.log(typeof(new String('TAP Air Portugal').slice(0)));
+
+
+//  1. Gets the index of letters and words
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));  //Gets the last index of the word in airline string
+console.log(airline.indexOf('Portugal'));  // Search where the word starts at the index of the string
+
+// 2. Slicing String
+//  - Begin parameter
+console.log(airline.slice(4)); //Slicing the string starting at the index, anything before it will be sliced off
+//  - End parameter
+//    length of sliced string = end parameter - begin parameter
+console.log(airline.slice(4, 7)); // returns Air , 4 is A and 7 is the blank space, so the slicing stops at 7 meaning 7 and anthing after it is sliced off
+
+// 3. Slicing without knowing the index of the string received
+//  - Extracting the first Word
+console.log(airline.slice(0, airline.indexOf(' ')));  // Start at index 0 and end at the index before the blank
+
+// - Extracting the Last Word
+console.log(airline.slice(airline.lastIndexOf(' ')));  // Slicing at the index that contains the last occuring blank
+console.log(airline.slice(airline.lastIndexOf(' ') + 1)); // Tanking out the blank 
+
+// - Counting from the end of the string (negative index)
+console.log(airline.slice(-2));   // last letter in the sting will be -1
+console.log(airline.slice(1, -1)); // Returns AP Air Portuga, slicing starts at index 1 and ends before the last letter of the string
+
+const checkMiddleSeat = function(seat) {
+  // B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') {
+    console.log('you have the middle seat')
+  } else {
+    console.log('You do not have the middle seat yay')
   }
 }
-
-const answer = Number(prompt(`Please choose an answer from number 1 to 3`));
-console.log(answer);
-// - MY WAY 
-// if (answer === question.get('correct')) {
-//   console.log(question.get(true));  
-// } else {
-//   console.log(question.get(false));  
-// };
-
-// - Better Solution (Having boolean values as key)
-// - Always work from the inner ( ) then work your way to the most outer (getting the logic of the inner () to work first then use the outer ())
-console.log(question.get(question.get('correct') === answer));
-
-
-// >> 4. Convert Map to Array (use unpacking ... operator on Map)
-console.log([...question]);  // Same is console.log([...question.entries()])
-console.log([...question.keys()]); // Unpack all the keys and store it into an array
-console.log([...question.values()]); // unpack all the values and store it into an array
-
-
-
-
-
-
-
-// Maps - Fundamentals
-// // >> Create a map  <SYNTAX:> new Map()
-// const rest = new Map(); // Create an empty map (initially)
-// const names = new Map();
-
-// // >> Fill up the map (Add element to data stucture)  <SYNTAX:> .set()
-// //  -  Sets contains Key and value pairs so Key and value need to be added at the same time
-// rest.set('name', 'Classico Italiano');
-// rest.set(1, 'Firenze, Itality');
-// console.log(rest.set(2, 'Lisbon, protugal')); // .set will update the most recent set with the new element
-// console.log(rest);
-
-// // >> Chaining Maps (Creating elements one after the next) <SYNTAX:> .set().set().set()  etc
-// rest
-//   .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
-//   .set('open', 11)
-//   .set('close', 23)
-//   .set(true, 'We are open')
-//   .set(false, 'we area closed');
-// console.log(rest);
-
-// // >> Read Data from maps  <SYNTAX:> .get()
-// //  - Pass in the name of the key
-// console.log(rest.get('name'));
-// console.log(rest.get(true));
-// console.log(rest.get(1));
-
-// // Realtime usage
-// const time = 21;
-// // this example is clever but code may not be easily readable
-// console.log(rest.get(time > rest.get('open') && time < rest.get('close'))); //Compare time against the time open and close in the map and return the value of true and false
-
-// // >> Check a map contains certain KEY <SYNTAX:> .has()
-// console.log(rest.has('categories')); // Returns True if it has the key and false if it doesnt have it
-
-// // >> Deleting element in a map using the KEY <SYNTAX;> .delete()
-// rest.delete(2);
-// console.log(rest);
-
-// // >> Check the size of the map <SYNTAX:> .size
-// console.log(rest.size);
-
-// // >> Remove all element from the map <SYNTAX:> .clear()
-// names.set('1', 'Jason').set(2, 'Faye');
-// console.log(names);
-// names.clear();
-// console.log(names);
-
-// // >> Using ARRAY and OBJECT and map KEYS
-// // ARRAY as KEY
-// rest.set([1, 2], 'Test');
-// console.log(rest);
-// // Retriving data using the KEY of ARRAY
-// console.log(rest.get([1, 2])); // This array has its own idenfier (in the Heap), not the same as the one we construct in the .set()
-
-// //To make the above work
-// // - An array new to be made
-// const arr = [1, 2]; // This will allow JS to always use the same array identifer in the Heap
-
-// // - Pass in the made array into the map
-// rest.set(arr, 'test1');
-// console.log(rest.get(arr));
-
-// // OBJECT as KEY (Using DOM object as Key) NOTE: DOM object is object type
-// rest.set(document.querySelector('h1'), 'Heading');
-// console.log(rest);
-
-
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
