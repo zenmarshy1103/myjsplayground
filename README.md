c68d3120b7a3fbf173e9f99583d6baae93661ed9
Jason Liu <104143535+zenmarshy1103@users.noreply.github.com>
2023-05-18 08:55:21 +1200

    Functions - Accepting Callback Functions
    // - Use a function to call back another function 
    // - HIGH ORDER FUNCTION (HIGH LEVEL Abstraction) - using function as one of its parameter, and the callback function is lower level abstration
    // - Abstraction Concept: Higher Order Function only cared about its own function rather than the function of the callback function

--- README.md
+++ README.md
@@ -187,6 +187,23 @@ Primitive vs Objects
         2. HEAP - where objects are stored in memory (Reference Types are stored in the MEMORY HEAP)
 
 
+ >> TODO: Need to format 
+ // Functions - First Class and Higher Order Functions (THEORY)
+// - FIRST CLASS FUNCTIONS:  (There are no FIRST CLASS FUNCTION in practice rather it is just a FEATURE of JavaScript)
+//      -> JavaScript treats functions as FIRST CLASS CITIZENS
+//      -> This means that functions are simple values
+//      -> FUnction are just another TYPE of OBJECT
+// - Function:
+//      -> Stores function in variable or properties
+//      -> Pass function as arguments to other functions
+//      -> Return functions from functions
+//      -> Call methods on functions.
+// - HIGHER ORDER FUNCTIONS:
+//      -> A function that receives another function as an argument, that returns a new function or both
+//      -> THis is only possible of FIRST CLASS FUNCTIONS
+//      -> ie: the .addEventLister('click', greet); is a higher order function as it receives in a call back function which is called upon later as soon as the click happens
+
+
 
 
 
--- script.js
+++ script.js
@@ -1,18 +1,42 @@
-// Functions - First Class and Higher Order Functions (THEORY)
-// - FIRST CLASS FUNCTIONS:  (There are no FIRST CLASS FUNCTION in practice rather it is just a FEATURE of JavaScript)
-//      -> JavaScript treats functions as FIRST CLASS CITIZENS
-//      -> This means that functions are simple values
-//      -> FUnction are just another TYPE of OBJECT
-// - Function:
-//      -> Stores function in variable or properties
-//      -> Pass function as arguments to other functions
-//      -> Return functions from functions
-//      -> Call methods on functions.
-// - HIGHER ORDER FUNCTIONS: 
-//      -> A function that receives another function as an argument, that returns a new function or both
-//      -> THis is only possible of FIRST CLASS FUNCTIONS
-//      -> ie: the .addEventLister('click', greet); is a higher order function as it receives in a call back function which is called upon later as soon as the click happens
+// Functions - Accepting Callback Functions
+// - Use a function to call back another function
+// - HIGH ORDER FUNCTION (HIGH LEVEL Abstraction ) - using function as one of its parameter, and the callback function is lower level abstration
+// - Abstraction  Concept: Higher Order Function only cared about its own function rather than the function of the callback function
 
 'use strict';
 
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase(); //replace spaces / /g   <- (g: global) with empty string
+};
 
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' '); // ... Spread Operator:  unpack the elements in the array that was made when .split() was called
+  return [first.toUpperCase(), ...others].join(' '); // joins the first and ...others by space
+};
+
+// >> 1. Higher Order Function
+//      - Takes in a function (the function value is passed in not function)
+//      - Function in general have property and build in method
+//      - Abstraction  Concept: Higher Order Function only cared about its own function rather than the function of the callback function
+const transformer = function (str, fn) {
+  console.log(`Original String: ${str}`);
+  console.log(`Transformed String: ${fn(str)}`); // fn(str)  - giving the input parater of str to the call back function input
+  console.log(`Transformed By: ${fn.name}`); //rading the name property of the function
+};
+
+// >> 2.  Call Back functions
+//      - Use a function to call back another function
+//      - JS uses callbacks all the time
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// function high5() {
+//     console.log(`🖐`);
+// }
+
+const high5 = function () {
+  console.log(`🖐`);
+};
+document.body.addEventListener('click', high5); // the function will be called back when the click event listener has been triggered
+
+['Jason', 'Faye', 'Cyrus'].forEach(high5); // for each of the element call the call back function on each of them
