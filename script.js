4bc960805824e1d097af5d8b5aa67bd2098bfc09
Jason Liu <104143535+zenmarshy1103@users.noreply.github.com>
2023-05-23 08:24:03 +1200

    Functions - The Call and Apply Method to this Keyword
    // - Setting this this keyword manually (using .call(), .apply() and .bind())
    //    - Call Method <SYNTAX:> function.call(this-keyword-pointing-to, param1, param2, etc)  <- MODERN JS
    //    - Apply Mehtod <SYNTAX:> function.apply(this-keyword-pointing-to, array of param)  <- Hardly Used Nowadays
    //    - Bind Method <SYNTAX:> function.bind(this-keyword-point to) and creates a new function, used for partial application purposes also

--- script.js
+++ script.js
@@ -1,48 +1,116 @@
-// Functions - Function returning Functions
-// - Useful when using Functional Programming Paradigm
-// - <Interpretation> 
-// -    1. Outer function is called first which returns a function
-// -    2. The outer function that returns a function then is invoked passing in argument for inner function
-
+// Functions - The Call and Apply Method to this Keyword
+// - Setting this this keyword manually (using .call(), .apply() and .bind())
+//    - Call Method <SYNTAX:> function.call(this-keyword-pointing-to, param1, param2, etc)  <- MODERN JS
+//    - Apply Mehtod <SYNTAX:> function.apply(this-keyword-pointing-to, array of param)  <- Hardly Used Nowadays
+//    - Bind Method <SYNTAX:> function.bind(this-keyword-point to) and creates a new function, used for partial application purposes also
 'use strict';
 
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  // book: function (){} // ES5 SYNTAX
+  book(flightNum, name) {
+    // ES6 SYNTAX
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
 
-// >> Closures 
-//    - Need to come back to this (Most misunderstood topic in JavaScript)
-const greet = function(greeting) {
-  return function(name) {
-    console.log(`${greeting} ${name}`);
+lufthansa.book(239, 'Jason Liu');
+lufthansa.book(635, 'John Smith');
+console.log(lufthansa);
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+const book = lufthansa.book; // Storing a method from an object to an variable, turning a method to a normal function then use it in other object as an method
+// <Note> Regular function call, this keyword points to undefined
+// book(23, 'Faye Jia')  //<- does not work
+
+// >> 1 .Setting this Keyword manually
+//      - Telling JaveScript what this should look like rather than getting undefined
+//      - Function is an object and object has methods, so function has build in methods.
+
+//  - Call Method
+//    - SYNTAX: function.call(THISKEYWORD-Pointing-to-object, argument to function)
+book.call(eurowings, 23, 'Faye Jia');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Cyrus Liu');
+console.log(lufthansa);
+
+//  - Apply Method (Not really used much nowadays)
+//    - will take an array of the argument
+const flightData = [583, 'Claire Liu'];
+book.apply(eurowings, flightData);
+console.log(eurowings);
+
+// >> Modern JS
+// - Use .call() method
+// - use ... spread operator on array to get each element variables
+book.call(swiss, ...flightData); // Using the spread method to use the elements in the array
+console.log(swiss);
+
+// - Bind Method
+// - Doex not call the function
+// - returns the new function where the 'this' keyword is bound
+// - It sets to what ever value we pass into .bind()
+const bookEW = book.bind(eurowings); // This does not call the book function but return a new function for book and the this keyword will be set to eurowings
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23, 'Steven Williams');
+
+// >> Resetting parameters of new function (Partial Application - part of the parameter is applied)
+const bookEW23 = book.bind(eurowings, 23); // Presetting the first parameter of the bookEW23 function. now book(flightNum, name) , has 23 set to FlightNum, so the new function only require a name parameter
+bookEW23('Jason Liu');
+bookEW23('Claire Liu');
+
+//  >> Using Object with Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa)); // this keyword in an event listener function will always point to the element calling the event listener
+  // So we need to bind the this keyword to lufthansa.
+
+// >> Partial Application (Preset Parameters)
+const addTax = (rate, value) => value + value * rate; //Arrow function returning value + value * rate (without return keyword)
+console.log(addTax(0.10, 200));
+
+const addVAT = addTax.bind(null, 0.23);  //setting null because we are not goint to assign the this keyword in this application but the parameres only, setting rate = 0.23 for the new addVat function
+console.log(addVAT(100));
+console.log(addVAT(50000));
+console.log(`----------------`)
+
+// Practice Example 
+// - Turning the Partial Application above using function returning function method 
+const addTaxRate = function(rate) {    // Box 1 contains Box 2. BOX 2 has the end product the final outcome of the functoin
+  return function(value) {
+    return value + value * rate;
   };
 };
 
-// >> Calling functions returning functions
-// 1. Method One - Unboxing each function
-// Theory: Think of the most outer function is the outer box containing an inner box
-//       1. Opening outer box (outer function), which contains the inner box
-//       2. Then open the inner box, which has the functionality of the function.
-const greeterHey = greet('Hey'); // The greet function returns an function and the value of greet is stored in greeterHey variable (This Variable now is a function returning another function)
-greeterHey('Jason');  //using the greeterHey functino with parameter Jason.
-greeterHey('faye')
-
-// 2. Method Two
-greet('Hello')('Jason');   //The correct interpretation is that the result of the expression greet('Hello') is a function, and then this function is immediately invoked with the argument 'Jason'.
-
-//<Explanation> 
-//To break it down step by step:
-
-// 1. greet('Hello') is called first, which returns a function.
-// 2. This returned function is then immediately invoked by using parentheses ( ), and the argument 'Jason' is passed to it.
-//   So, ('Jason') is not a separate function call, but rather the argument passed to the function returned by greet('Hello').
-
-// Task: Turn greet function into arrow function
-const greetArrow = greeting => {
-  return name => {
-    console.log(`${greeting} ${name}`);
-      };
-}; 
-
-// Shorter Way
-const greetArr = greeting => name => console.log(`${greeting} ${name}`);
-
-greetArrow('Whats Up')('Jason');
-greetArr('Howdy')('Faye');
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT(100));
+console.log(addVAT(50000));
+console.log(`----------------`)
+console.log(addTaxRate(0.23)(100));
+console.log(addTaxRate(0.23)(50000));
