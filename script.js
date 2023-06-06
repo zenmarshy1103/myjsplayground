5ae1ab90e6ce2b0b0841c13bbb1bfc4a81a69f8f
Jason Liu <104143535+zenmarshy1103@users.noreply.github.com>
2023-06-07 08:12:05 +1200

    ARRAY - DATA Transformation with Map, Filter and Reduce (map method)
    // - Maps: Return a new array containing the results of applying an operation on all original array elements
    // - Filter: Return a new array containing the array elements that passed a specified test condition
    // - Reduce: Boils, reduces all array elements  down to one single value (ie adding all elements together and other methods)

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// Arrays - Looping Arrays: forEach() method with Maps and Sets
-//  - <SYNTAX>  
-//    - Map) map.forEach(function(value, key, map){})
-//    - Set) set.forEach(function(value, key, map){})  <---- there is no key in sets so
-//    - Set) set.forEach(function(value, _, map){})
-//  - The reason for the similar syntax because the developer making these method did not want to create separate method for map and set.
+// ARRAY - DATA Transformation with Map, Filter and Reduce (map method)
+// - Maps: Return a new array containing the results of applying an operation on all original array elements
+// - Filter: Return a new array containing the array elements that passed a specified test condition
+// - Reduce: Boils, reduces all array elements  down to one single value (ie adding all elements together and other methods)
 'use strict';
 
 /////////////////////////////////////////////////
@@ -67,6 +65,34 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+// Functions
+const displayMovements = function (movementsArray) {
+  // Clear previous data in the movement container
+  containerMovements.innerHTML = '';
+
+  // Using For Each array method to get the value of each of the element and the index number
+  movementsArray.forEach(function (movementValue, valueIndex) {
+    // Tenery Operator to check if the movementValue is deposits or withdraw
+    const movementType = movementValue > 0 ? `deposit` : `withdrawal`;
+
+    // Create HTML Literals
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${movementType}">${
+      valueIndex + 1
+    } ${movementType}</div>
+      <div class="movements__value">${movementValue}€</div>
+    </div>  
+    `;
+
+    // Insert HTML Literals onto the DOM into movement container
+    // Using  insertAdjacentHTML method with afterbegin
+    //  - This will put the HTML literal with the last on the top of the beginning of the movement container html element
+    containerMovements.insertAdjacentHTML(`afterbegin`, html);
+  });
+};
+displayMovements(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -77,29 +103,55 @@ const inputClosePin = document.querySelector('.form__input--pin');
 //   ['GBP', 'Pound sterling'],
 // ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
-// >> forEach() method on Map
-console.log(`---------- forEach() on Map ----------`)
-const currencies = new Map([
-  ['USD', 'United States dollar'],  // Key: 'USD' Value: 'United States dollar'
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
 
-currencies.forEach(function(value, key, map){
-  console.log(`${key}: ${value}`);
-})
+// >> MAP Method
+//  - Map Method is functional paradigm (Functional Programming)
+//  - Each time the MAP method is calls a callback function:
+//    1) It will pass in: current Array element, current index and the whole array
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-// >> forEach() methods on Set
-// - <RECAP> Set only store the unique onces repeated ones gets deleted
-console.log(`---------- forEach() on Set ----------`)
-const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'TWD', 'EUR', 'TWD', 'EUR' ]);
-console.log(currenciesUnique);
-currenciesUnique.forEach(function(value, _, map){  // > JS:  _   throwaway varaible: Variables that are completely unnessessary 
-  console.log(`${key}: ${value}`);
+//Convert elements in movements array to USD
+
+const eurToUsd = 1.1;
+
+// const movementsUsd = movements.map(function(movementArray){
+//   return movementArray * eurToUsd;   // Returns an new array with each element * eurToUsd
+// })
+// console.log(movements);
+// console.log(movementsUsd);
+
+// Simply the callback function to arrow function
+//  - No parentheses is needed when there is only one argument for the arrow function
+//  - No need return statement and { } as we only have one line of code for return value
+const movementsUsd = movements.map(movementArray => movementArray * eurToUsd); // Returns an new array with each element * eurToUsd
+
+console.log(movements);
+console.log(movementsUsd);
+
+// Using for of loop to perform the same task
+const movementsUSDfor = [];
+for (const movement of movements) {
+  movementsUSDfor.push(movement * eurToUsd);
+}
+console.log(movementsUSDfor);
+
+// const movementDescriptions = movements.map((movement, i, arr) => {
+//   if (movement > 0) {
+//     return `Movement ${i + 1}: you deposited ${movement}`;
+//   } else {
+//     return `Movement ${i + 1}: you withdrew ${Math.abs(movement)}`;
+//   }
+// });
+// console.log(movementDescriptions);
+
+//  Better Way - Using Tenary Operator
+const movementDescriptions = movements.map((movement, i) => {  
   
+  return `Movement ${i + 1}: you ${movement > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+    movement
+  )}`;
 });
-
-
+console.log(movementDescriptions);
